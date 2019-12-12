2699586037849efa2f251156e6397480c4b8d03d
dessyd <dominique.dessy@gmail.com>
2019-12-12 17:52:19 +0100

    empty arduino_Secrtes.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-#define SECRET_SPLUNK_SERVER ""
+#define SECRET_SSID "Mobistar-B805"
+#define SECRET_PASS "3676093595453896"
+#define SECRET_SPLUNK_SERVER "gurga.synology.me"
